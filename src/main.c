4f3e145033689e6261e8e4c19cbf11fc98fb6a59
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2017-01-07 20:25:54 +0100

    Use the Readline library

--- src/main.c
+++ src/main.c
@@ -1,12 +1,23 @@
 #include <time.h>
 #include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include "btree.h"
 
 int main() {
 	srand(time(NULL));
-
 	Btree *btree = btree_new("btree.dat");
-	btree_destroy(btree);
 
+	char *line = NULL;
+	while ((line = readline("btree> "))) {
+		if (line[0] != '\0')
+			add_history(line);
+
+		printf("You entered: %s\n", line);
+
+		free(line);
+	}
+
+	btree_destroy(btree);
 	return 0;
 }
