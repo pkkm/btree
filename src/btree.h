a5f93bb1d19fdc40a89fdcd7fb3006c4335b4a99
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2017-01-08 19:04:00 +0100

    Expose B-tree operations in btree.h

--- src/btree.h
+++ src/btree.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <inttypes.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 typedef uint32_t BtreeKey;
 typedef uint64_t BtreeValue;
@@ -10,3 +12,10 @@ typedef struct Btree Btree;
 
 Btree *btree_new(const char *file_name);
 void btree_destroy(Btree *btree);
+
+bool btree_get(Btree *btree, BtreeKey key, BtreeValue *value);
+void btree_set(Btree *btree, BtreeKey key, BtreeValue value);
+
+void btree_print(Btree *btree, FILE *stream);
+void btree_walk(Btree *btree, void (*callback)(BtreeKey, BtreeValue, void *),
+                void *callback_context);
